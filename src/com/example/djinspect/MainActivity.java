2784f25201c8ab83679862fba6f20ab7e1ae151c
gbh <thebhef@gmail.com>
2013-12-27 03:07:07 -0600

    front camera now. autoformatted

--- src/com/example/djinspect/MainActivity.java
+++ src/com/example/djinspect/MainActivity.java
@@ -6,14 +6,17 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
-import org.opencv.core.Mat;
-import org.opencv.highgui.Highgui;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+
+
+
 
 public class MainActivity extends Activity implements CvCameraViewListener2 {
 
@@ -30,14 +33,13 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
 		setContentView(R.layout.activity_main);
-		
+
 		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.camView);
-		mOpenCvCameraView.setCameraIndex(1);
+		mOpenCvCameraView.setCameraIndex(0); // 0 for back, 1 for front on gs3
 		mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
 		mOpenCvCameraView.setCvCameraViewListener(this);
 
 	}
-
 
 	public void onCameraViewStarted(int width, int height) {
 		Log.i("CameraStart", "camera started");
@@ -46,51 +48,52 @@ public class MainActivity extends Activity implements CvCameraViewListener2 {
 	public void onCameraViewStopped() {
 		Log.i("CameraStop", "camera stopped");
 	}
-
+	
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
 		Log.i("CameraFrame", "new frame");
-		return inputFrame.rgba();
+		Mat rval = inputFrame.rgba();
+			
+		return rval;
 	}
-	
-    @Override
-    public void onPause()
-    {
-        super.onPause();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
 
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this, mLoaderCallback);
-    }
-    
-    public void onDestroy() {
-        super.onDestroy();
-        if (mOpenCvCameraView != null)
-            mOpenCvCameraView.disableView();
-    }
-    
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+	@Override
+	public void onPause() {
+		super.onPause();
+		if (mOpenCvCameraView != null)
+			mOpenCvCameraView.disableView();
+	}
 
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    Log.i("ManagerConnected", "OpenCV loaded successfully");
+	@Override
+	public void onResume() {
+		super.onResume();
+		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, this,
+				mLoaderCallback);
+	}
 
-                    /* Now enable camera view to start receiving frames */
-                    mOpenCvCameraView.enableView();
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
-            }
-        }
-    };
-		
+	public void onDestroy() {
+		super.onDestroy();
+		if (mOpenCvCameraView != null)
+			mOpenCvCameraView.disableView();
+	}
+
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+
+		@Override
+		public void onManagerConnected(int status) {
+			switch (status) {
+			case LoaderCallbackInterface.SUCCESS: {
+				Log.i("ManagerConnected", "OpenCV loaded successfully");
+
+				/* Now enable camera view to start receiving frames */
+				mOpenCvCameraView.enableView();
+			}
+				break;
+			default: {
+				super.onManagerConnected(status);
+			}
+				break;
+			}
+		}
+	};
+
 }
